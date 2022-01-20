0a1b148045d6b9d858668ed2f606a970603397cc
denisesenguel <denisesenguel@gmail.com>
2022-01-20 17:03:04 +0100

    add obstacles moving down in one place

--- js/game.js
+++ js/game.js
@@ -1,10 +1,41 @@
 class Game {
 
+    constructor() {
+        this.obstacles = [];
+        this.timer = 0;
+    }
+
     start() {
+
+        // create player
         this.player = new Player();
+        console.log(this.player.position);
         this.player.domElement = this.createDomElm(this.player);
         this.drawDomElm(this.player);
+        
+        // allow player to move
         this.addEventListeners();
+
+        // add and move obstacles 
+        setInterval(() => {
+            
+            if (this.timer % 5 == 0) {
+
+                // create new obstacle
+                const newObstacle = new Obstacle();
+                newObstacle.domElement = this.createDomElm(newObstacle);
+                this.drawDomElm(newObstacle);
+                this.obstacles.push(newObstacle);
+            }
+            
+            // move all obstacles
+            this.obstacles.forEach(obstacle => {
+                obstacle.moveDown();
+                this.drawDomElm(obstacle);
+            });
+            this.timer++;
+        }, 500);
+
     }
 
     addEventListeners() {
@@ -44,14 +75,22 @@ class Game {
     }
 }
 
-class Player {
-    constructor() {
-        this.className = "player";
+class boardObject {
+
+    constructor(type) {
+        this.className = type;
         this.position = {x: 0, y: 0};
         this.speed = 10;
         this.width = 10;
         this.height = 10;
         this.domElement = null;
+    }
+}
+
+class Player extends boardObject {
+
+    constructor() {
+        super('player');
     }
 
     moveLeft() {
@@ -60,6 +99,18 @@ class Player {
 
     moveRight() {
         this.position.x += this.speed;
+    }
+}
+
+class Obstacle extends boardObject {
+
+    constructor() {
+        super('obstacle');
+        this.position = {x: 50, y: 0};
+    }
+
+    moveDown() {
+        this.position.y += this.speed;
     }
 }
 
